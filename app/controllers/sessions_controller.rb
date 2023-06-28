b442e4e88e7480f7ee8e39f7fef13b2611fcfe97
Ubuntu <ubuntu@ip-172-31-24-112.ap-northeast-1.compute.internal>
2023-06-28 12:15:35 +0000

    advancelogin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       log_in(user)
       redirect_to user
     else
--- db/migrate/20230604235703_add_password_digest_to_users.rb
+++ db/migrate/20230604235703_add_password_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :password_digest, :string
+    rails generate migration add_remember_digest_to_users remember_digest:string
   end
 end
